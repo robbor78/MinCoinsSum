76de93d1305f73110a793da3b0bfdf04659d67ff
username <email@email.com>
2016-02-26 14:21:52 +0200

    min coins sums topcoder

---
+++ MinCoinsSum/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinCoinsSum
+{
+  /*
+  https://www.topcoder.com/community/data-science/data-science-tutorials/dynamic-programming-from-novice-to-advanced/
+    Given a list of N coins, their values (V1, V2, … , VN), and the total sum S.
+  Find the minimum number of coins the sum of which is S 
+  (we can use as many coins of one type as we want),
+  or report that it’s not possible to select coins in such a way that they sum up to S.*/
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    /// <summary>
+    /// Start with lowest state (0) and increment to desired state (sum).
+    /// </summary>
+    /// <param name="coins"></param>
+    /// <param name="sum"></param>
+    /// <returns></returns>
+    public int findMinCoinsSum1(int[] coins, int sum)
+    {
+      /*Set Min[i] equal to Infinity for all of i
+Min[0]=0
+
+For i = 1 to S
+For j = 0 to N - 1
+   If (Vj<=i AND Min[i-Vj]+1<Min[i])
+Then Min[i]=Min[i-Vj]+1
+
+Output Min[S]*/
+
+      long[] min = Enumerable.Repeat((long)int.MaxValue, sum + 1).ToArray();
+      min[0] = 0;
+
+      int numCoins = coins.Length;
+      for (int i = 1; i <= sum; i++)
+      {
+        for (int j = 0; j < numCoins; j++)
+        {
+          int valuej = coins[j];
+          if (valuej <= i && min[i - valuej] + 1 < min[i])
+          {
+            min[i] = min[i - valuej] + 1;
+          }
+        }
+      }
+
+      return min[sum] == int.MaxValue ? -1 : (int)min[sum];
+    }
+
+    /// <summary>
+    /// Iteratively improve on existing solutions
+    /// </summary>
+    /// <returns></returns>
+    public int findMinCoinsSum2(int[] coins, int sum)
+    {
+      long[] min = Enumerable.Repeat((long)int.MaxValue, sum + 1).ToArray();
+      min[0] = 0; //first solution
+      long maxValue = 0;
+
+      bool isValueTooLarge = false;
+      while (maxValue < sum && !isValueTooLarge)
+      {
+        long tmpMaxValue = maxValue;
+        for (int c = 0; c < coins.Length; c++)
+        {
+          for (int m = 0; m <= tmpMaxValue; m++)
+          {
+
+            long value = coins[c] + m;
+            if (value > sum)
+            {
+              isValueTooLarge = true;
+              continue;
+            }
+            long currentMin = min[value];
+            long possibleNewMin = min[m] + 1;
+            if (currentMin > possibleNewMin)
+            {
+              min[value] = possibleNewMin;
+              if (value > maxValue)
+              {
+                isValueTooLarge = false;
+                maxValue = value;
+              }
+            }
+          }
+        }
+      }
+
+      return min[sum] == int.MaxValue ? -1 : (int)min[sum];
+    }
+
+
+  }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinCoinsSum;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      int[][] allCoins = new int[][]
+      {
+        new int [] { 1,3,5},
+        new int [] { 5,10,20,50},
+
+      };
+
+      int[] allSums = new int[] { 11, 7 };
+
+      int[] allMinSums = new int[] { 3, -1 };
+
+      Program p = new Program();
+      int length = allCoins.Length;
+      for (int i = 0; i < length; i++)
+      {
+        int[] coins = allCoins[i];
+        int sum = allSums[i];
+
+        int actual = p.findMinCoinsSum2(coins, sum);
+
+        int expected = allMinSums[i];
+
+        Assert.AreEqual(expected, actual);
+      }
+    }
+  }
+}
